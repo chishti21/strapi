e37d5f83088ca6e3ac155a14e6be8af94824281d
chishti21 <webandgraphicsdesignhouse@gmail.com>
2025-09-02 20:27:32 +0500

    update database config for postgres

---
+++ config/env/prduction/database.ts
@@ -0,0 +1,14 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+    pool:{min:0}
+  },
+});
